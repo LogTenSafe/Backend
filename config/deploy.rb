8cc1ddc72d72c139f0c4b7f9f6179ddf01397614
Tim Morgan <github@timothymorgan.info>
2015-11-06 16:41:08 -0800

    Ruby and Rails updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'logtensafe'
 set :repo_url, 'git://github.com/LogTenSafe/website.git'
 
 set :deploy_to, '/var/www/www.logtensafe.com'
-set :rvm_ruby_version, "2.1.5@#{fetch :application}"
+set :rvm_ruby_version, "2.2.2@#{fetch :application}"
 
 set :linked_files, %w{config/environments/production/secrets.yml config/secrets.yml config/database.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
