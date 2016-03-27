a53a6fb7832ebe988bf49047408af5288838929c
Tim Morgan <github@timothymorgan.info>
2016-03-26 19:44:49 -0700

    New server config

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git://github.com/LogTenSafe/website.git'
 set :deploy_to, '/var/www/www.logtensafe.com'
 set :rvm_ruby_version, "2.3.0@#{fetch :application}"
 
-set :linked_files, %w{config/environments/production/secrets.yml config/secrets.yml config/database.yml}
+set :linked_files, %w{config/environments/production/secrets.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,3 @@ set :stage, :production
 role :app, %w{www-data@www.logtensafe.com}
 role :web, %w{www-data@www.logtensafe.com}
 role :db,  %w{www-data@www.logtensafe.com}
-
-# http://blog.manzhikov.com/new-passenger-restart-in-5-version
-set :rvm_map_bins, fetch(:rvm_map_bins, []).push('rvmsudo')
-set :passenger_restart_command, 'rvmsudo passenger-config restart-app'
