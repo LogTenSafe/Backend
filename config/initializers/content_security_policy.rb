f3be07d0a045235225b280ce9c21b67d49a175e5
Tim Morgan <github@timothymorgan.info>
2019-04-27 15:51:15 -0700

    Tighten content security policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,20 +4,30 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+extra_script_sources = %w[
+  https://maxcdn.bootstrapcdn.com
+  https://code.jquery.com
+  https://cdnjs.cloudflare.com
+]
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self
+  policy.font_src    :self, :data
+  policy.img_src     :self, :data
+  policy.object_src  :none
+  policy.script_src  :self, *extra_script_sources
+  policy.style_src   :self, :unsafe_inline, 'https://maxcdn.bootstrapcdn.com'
+
+  if Rails.env.development?
+    policy.connect_src :self, 'ws://localhost:3035', 'http://localhost:3035'
+  end
+
+  # Specify URI for violation reports
+  # policy.report_uri "/csp-violation-report-endpoint"
+end
 
 # If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+Rails.application.config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
