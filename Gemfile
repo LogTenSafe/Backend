a553b8ab77db6f5bae1653877bd90a319f2c3619
Tim Morgan <tmorgan@onemedical.com>
2020-04-02 15:01:10 -0700

    Ruby and Rails update

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 # FRAMEWORK
 gem 'bootsnap'
 gem 'configoro'
-gem 'rails', '6.0.1'
+gem 'rails', '6.0.2.2'
 
 # CONTROLLERS
 gem 'responders'
--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, 'logtensafe'
 set :repo_url, 'https://github.com/LogTenSafe/website.git'
 
 set :deploy_to, '/var/www/www.logtensafe.com'
-set :rvm_ruby_version, "2.7.0@#{fetch :application}"
+set :rvm_ruby_version, "2.7.1@#{fetch :application}"
 
 append :linked_files, 'config/master.key', 'config/sidekiq.yml'
 
